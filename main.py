a878451d7886b1080568640788bfb36a83bdcc92
Ilya Tokarev <tokarevid2004@gmail.com>
2026-04-04 22:17:22 +0500

    bugfix

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def process_endpoint(
         input_file: str = Query(None, description="Путь к файлу, если use_latest=False"),
         results_dir: str = Query("results", description="Папка для сохранения результатов"),
 
-        voxel_size: float = Query(1.0, description="Размер вокселя для даунсэмплинга"),
+        voxel_size: float = Query(0.0, description="Размер вокселя для даунсэмплинга"),
         nb_neighbors: int = Query(20, description="Количество соседей для фильтрации шума"),
         std_ratio: float = Query(2.0, description="Коэффициент стандартного отклонения при фильтрации"),
         distance_threshold: float = Query(70.0, description="Порог расстояния при удалении плоскости"),
